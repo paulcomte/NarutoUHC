730cdaa5e03da2e2795fd5d780525b6b30a1d33a
RqndomHax <paulcomte1@gmail.com>
2021-01-08 19:34:01 +0100

    Added roles enum

---
+++ src/fr/rqndomhax/narutouhc/role/RoleType.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021.
+ *  Discord : _Paul#6918
+ *  Author : RqndomHax
+ *  Github: https://github.com/RqndomHax
+ */
+
+package fr.rqndomhax.narutouhc.role;
+
+public enum RoleType {
+    SHINOBI,
+    AKATSUKI,
+    OROCHIMARU,
+    SASUKE,
+    DANZO;
+}
---
+++ src/fr/rqndomhax/narutouhc/role/Roles.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021.
+ *  Discord : _Paul#6918
+ *  Author : RqndomHax
+ *  Github: https://github.com/RqndomHax
+ */
+
+package fr.rqndomhax.narutouhc.role;
+
+public enum Roles {
+    
+    MADARA(RoleType.SASUKE),
+    SASUKE(RoleType.SASUKE),
+
+    DANZO(RoleType.DANZO),
+
+    DEIDARA(RoleType.AKATSUKI),
+    NAGATO(RoleType.AKATSUKI),
+    Kakuzu(RoleType.AKATSUKI),
+    KISAME(RoleType.AKATSUKI),
+    OBITO(RoleType.AKATSUKI),
+    KONAN(RoleType.AKATSUKI),
+    HIDAN(RoleType.AKATSUKI),
+
+    NARUTO(RoleType.SHINOBI),
+    KAKASHI_HATAKE(RoleType.SHINOBI),
+    SHIKAMARU(RoleType.SHINOBI),
+    HINATA(RoleType.SHINOBI),
+    MINATO(RoleType.SHINOBI),
+    GAI(RoleType.SHINOBI),
+    NEJI(RoleType.SHINOBI),
+    SHISUI(RoleType.SHINOBI),
+    SAKURA(RoleType.SHINOBI),
+    ITACHI(RoleType.SHINOBI),
+    KILLER_BEE(RoleType.SHINOBI),
+    GAARA(RoleType.SHINOBI),
+    FU(RoleType.SHINOBI),
+
+    OROCHIMARU(RoleType.OROCHIMARU),
+    KABUTO(RoleType.OROCHIMARU);
+    private final RoleType roleType;
+
+    Roles(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+}
